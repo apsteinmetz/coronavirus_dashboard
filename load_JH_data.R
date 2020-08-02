a82f886920fac139ff39cb751e48781b5833c2e4
apsteinmetz <apsteinmetz@yahoo.com>
2020-08-02 15:35:32 -0400

    add to plots

--- load_JH_data.R
+++ load_JH_data.R
@@ -4,14 +4,21 @@ library(tidyverse)
 # using the three CSV files you will find here
 #https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
 date_regex <- "^\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}$"
-#git_dir <- "~\\GitHub\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_19-covid-"
-git_dir <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-"
-# csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
-types <- as.factor(c("Confirmed","Deaths","Recovered"))
 
+# local repo
+git_dir <- "~\\GitHub\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_"
+
+# direct from github. not working now
+#git_dir <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_"
+
+fname <- paste0(git_dir,"confirmed_global.csv")
+
+types = c("confirmed","deaths")
 
 cv_raw <- types %>% purrr::map(function(type_name){
-  read_csv(paste0(git_dir,type_name,".csv")) %>% 
+  fname <- paste0(git_dir,type_name,"_global.csv")
+  print(fname)
+  read_csv(fname) %>% 
     mutate(type = type_name)
 }) %>% 
   bind_rows() %>% 
@@ -23,6 +30,7 @@ cv <- cv_raw %>% rename(Country.Region = `Country/Region`,Province.State = `Prov
 
 
 cv_mod <- cv %>%   
+  filter(!is.na(cases)) %>% 
   mutate(region = Country.Region) %>% 
   mutate(region = if_else(Country.Region == "United Arab Emirates", "UAE", region)) %>%
   mutate(region = if_else(Country.Region == "China" & Province.State != "Hubei", "China ex Hubei", region)) %>%
@@ -31,5 +39,16 @@ cv_mod <- cv %>%
   # change US counties to state
   mutate(Province.State = ifelse(str_ends(Province.State,", [A-Z]{2}"),
                                                 state.name[match(str_extract(Province.State,"[A-Z]{2}$"),state.abb)],
-                                                Province.State))
+                                                Province.State)) %>% 
+  {.}
+
+cv_us_states <- cv %>% 
+  filter(Country.Region == "US") %>%
+  mutate(state = ifelse(str_ends(Province.State,", [A-Z]{2}"),
+                                 state.name[match(str_extract(Province.State,"[A-Z]{2}$"),state.abb)],
+                                 Province.State)) %>% 
+  mutate(county = ifelse(str_ends(Province.State,", [A-Z]{2}"),
+                         str_extract(Province.State,"^[a-zA-Z]+"),
+                         NA)) %>% 
+  {.}
 
--- plot_cases.R
+++ plot_cases.R
@@ -9,62 +9,63 @@ library(ggplot2)
 # get johns hopkins data into a tibble called cv_mod
 source("load_JH_data.r")
 
-pop <- readr::read_csv("data/demographic-2019-UNFPA.csv") %>% 
-  rename(region = `Countries and areas`) %>% 
-  rename(population = 'Total population in millions, 2019') %>% 
-  select(region,population) %>% 
-  mutate(population= population * 1e6)
+# pop <- readr::read_csv("data/demographic-2019-UNFPA.csv") %>% 
+#   rename(region = `Countries and areas`) %>% 
+#   rename(population = 'Total population in millions, 2019') %>% 
+#   select(region,population) %>% 
+#   mutate(population= population * 1e6)
 
 # choose type
-# type_selection ="Deaths"
+type_select ="confirmed"
 # case_threshold = 10
 # yaxis_type = "log"
 
 countries_of_interest = c("Australia","Belgium","Denmark","France","Germany", "China ex Hubei",
                           "Hong Kong","Iran","Italy","Korea, South","United Kingdom","Singapore","Japan", "US")
 
-countries_of_interest_small = c("France", "China ex Hubei","Iran","Italy",
+countries_of_interest_small = c("France", "China ex Hubei","Iran","Italy","Spain",
                                 "Korea, South","United Kingdom","Japan", "US","Germany")
-# cum_cases %>% 
-#   # filter smaller, non-asian and china
-#   filter(region %in% countries_of_interest) %>% 
-#   ggplot(aes(days_since,cum_cases,color=region)) + 
-#   geom_line() + 
-#   scale_y_log10() + 
-#   NULL
 
-# type_select <- "Deaths"
-# countries <- countries_of_interest
-# #make cumulative cases
-# cum_cases <- cv_mod %>%
-#   filter(region == "Germany") %>% 
-#   filter(type == type_select) %>% 
-#   group_by(region, date) %>% 
-#   summarise(cases = sum(cases)) %>% 
-# #  mutate(cum_cases = cumsum(cases)) %>%
-#   mutate(cum_cases = cases) %>%
-#   filter(cum_cases > case_threshold) %>%
-#   group_by(region) %>%
-#   mutate(days_since = as.numeric(date-min(date))) %>%
-#   # left_join(pop) %>%
-#   # mutate(cum_cases_per100k = cum_cases/population*100000)
-#   {.}
-# 
-# cum_cases_wide <<- cum_cases %>% 
-#   # filter smaller, non-asian and china
-#   filter(region %in% countries) %>% 
-#   tidyr::pivot_wider(id_cols="days_since",names_from = "region",values_from = "cum_cases")
+countries_of_interest_recoveries <- cv_mod %>% 
+  filter(type == "Recovered",cases >200 ) %>% 
+  select(region) %>% 
+  unique() %>% 
+  pull()
 
+# ----------------------------------------
+# just for testing
+countries <- countries_of_interest
+case_threshold = 100
+type_select <- "confirmed"
+cum_cases <- cv_mod %>%
+  filter(region %in% countries) %>% 
+  filter(type == type_select) %>% 
+  group_by(region, date) %>% 
+  summarise(cases = sum(cases)) %>% 
+  #    mutate(cum_cases = cumsum(cases)) %>%
+  # cases from JH data are already cumulative
+  mutate(cum_cases = cases) %>%
+  filter(cum_cases > case_threshold) %>%
+  group_by(region) %>%
+  mutate(days_since = as.numeric(date-min(date))) %>%
+  # left_join(pop) %>%
+  # mutate(cum_cases_per100k = cum_cases/population*100000)
+  {.}
+# ----------------------------------------
+doubling_amt <- function(initial=1,time=1,doubling_time=3){
+  return (initial *2 ^(time/doubling_time))
+}
+# ----------------------------------------
 
-plot_cum_trend <- function(countries = "US",
-                           type_select = "Confirmed",
+plot_cum_trend <- function(regions = "US",
+                           type_select = "confirmed",
                            case_threshold = 10,
-                           scale = c("log","linear"),
+                           scale_type = "log", #could be linear
                            max_days = Inf){
   # Plotting the data
   #make cumulative cases
   cum_cases <- cv_mod %>%
-    filter(region %in% countries) %>% 
+    filter(region %in% regions) %>% 
     filter(type == type_select) %>% 
     group_by(region, date) %>% 
     summarise(cases = sum(cases)) %>% 
@@ -79,32 +80,49 @@ plot_cum_trend <- function(countries = "US",
     {.}
 
     cum_cases_wide <- cum_cases %>% 
-    # filter smaller, non-asian and china
     tidyr::pivot_wider(id_cols="days_since",names_from = "region",values_from = "cum_cases")
   
   # constrain the chart a bit
   cum_cases_wide <- cum_cases_wide %>% filter(days_since < max_days + 1)
   
   plot_object <- cum_cases_wide %>% 
-    plotly::plot_ly(x = ~days_since,y = ~US,type = "scatter",mode="lines+markers",name="US") %>% 
-    plotly::layout(yaxis = list(type = yaxis_type)) %>% 
-    {.}
-  
-  countries[which(countries != "US")] %>% 
+    plotly::plot_ly() %>% 
+    plotly::layout(yaxis = list(type = scale_type))
+  # make US stand out with markers and lines
+  if ("US" %in% countries){
+    plot_object <- plot_object %>%  
+      plotly::add_trace(x = ~days_since,y = ~US,type = "scatter",mode="lines+markers",name="US")
+    
+  } 
+    
+  regions[which(regions != "US")] %>% 
     purrr::walk( function(region) {
       print(region)
       plot_object <<- plot_object %>%
         plotly::add_trace(x = ~days_since,
                           y =  cum_cases_wide[[region]], 
-                          # type = "scatter", 
+                          type = "scatter", 
                           mode = "lines",
                           name = region)
     })
+  # add a growth rate constant line
+  plot_object <- plot_object %>% 
+    plotly::add_trace(x = ~days_since,y = doubling_amt(case_threshold,1:(max(cum_cases_wide$days_since)+1)),
+                      type = "scatter",mode="lines",line = list(dash="dash",color="black"),name="Doubling Time = 3 Days")
   
-  plot_object %>% plotly::layout(title = "",
+  # add a growth rate constant line
+  plot_object <- plot_object %>% 
+    plotly::add_trace(x = ~days_since,
+                      y = doubling_amt(case_threshold,1:(max(cum_cases_wide$days_since)+1),doubling_time = 2),
+                      type = "scatter",
+                      mode="lines",
+                      line = list(dash="dash",color="red"),
+                      name="Doubling Time = 2 Days")
+
+    plot_object %>% plotly::layout(title = "",
                                  legend = list(x = 0.9, y = 0.7),
-                                 yaxis = list(title = paste0("Cumulative ",type," Cases")),
-                                 xaxis = list(title = paste0("Days Since ",case_threshold," ",type)),
+                                 yaxis = list(title = paste0("Cumulative ",type_select," Cases")),
+                                 xaxis = list(title = paste0("Days Since ",case_threshold," ",type_select)),
                                  # paper_bgcolor = "black",
                                  # plot_bgcolor = "black",
                                  # font = list(color = 'white'),
@@ -119,6 +137,11 @@ plot_cum_trend <- function(countries = "US",
   
 }
 
-plot_cum_trend(countries_of_interest_small,"Deaths",10)
-plot_cum_trend(countries_of_interest,"Confirmed",100)
+plot_cum_trend(countries_of_interest_small,"deaths",10,max_days = 30)
+plot_cum_trend(countries_of_interest,"confirmed",100, max_days = 30)
+# plot_cum_trend(countries_of_interest_recoveries,"Recovered",10)
 
+
+# ----------------------------------------------------
+# daily cases
+daily_cases <- cv_mod %>% group_by(date,region,type) %>% mutate(daily_cases = cases - lag(cases))
