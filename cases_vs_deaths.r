196d0f09075164a7754e24cf7f49890f297d10bb
apsteinmetz <apsteinmetz@yahoo.com>
2020-08-02 16:21:48 -0400

    update

--- cases_vs_deaths.r
+++ cases_vs_deaths.r
@@ -1,6 +1,8 @@
 # correlate deaths and cases by state
 library(tidyverse)
-us_states <- read_csv("data/us-states.csv")
+
+# source https://github.com/nytimes/covid-19-data.git
+us_states <- read_csv("~/R Projects/covid-19-data/us-states.csv")
 
 # create rolling average changes and lags
 us_states <- us_states %>% 
@@ -19,22 +21,25 @@ us_states %>%
   theme(legend.position = "none") +
   geom_line(aes(y=deaths*20),color="red")
 
-coeff = 15
+
+coeff = 40
 us_states %>%  
   #filter(state %in% c("Florida","Texas","California","New York")) %>% 
   filter(state == state1) %>%
   ggplot(aes(date,cases_7day)) + geom_point(color="orange") +
-  geom_point(aes(y=d7*coeff),color="red") +
+  geom_point(aes(y=d0*coeff),color="red") +
   scale_y_continuous(
     name = "Cases",
-    sec.axis = sec_axis(d7~./coeff,name="Deaths")) +
+    sec.axis = sec_axis(d0~./coeff,name="Deaths")) +
   theme(
     axis.title.y = element_text(color = "orange", size=13),
     axis.title.y.right = element_text(color = "red", size=13)
   ) + 
   labs(title = paste(state1, "7-Day Moving Average"),
-       subtitle = "Deaths lagged by 7 Days",
+#       subtitle = "Deaths lagged by 17 Days",
        x = "2020") +
+  geom_vline(xintercept = as.Date("2020-07-16")) + 
+  geom_vline(xintercept = as.Date("2020-07-16")+17) + 
   NULL
 
 us_states %>%  
